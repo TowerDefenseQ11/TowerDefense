30b96d9c541a8a47befed23d882cfcbdc376986d
Manuel Selch <manuel@selch.de>
2023-06-23 12:21:46 +0200

    ios build without responsive screen

--- src/main/java/com/towerdefense/engine/endGUI.java
+++ src/main/java/com/towerdefense/engine/endGUI.java
@@ -1,6 +1,9 @@
 package com.towerdefense.engine;
 
+import com.towerdefense.map.Map;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.*;
@@ -9,28 +12,29 @@ public class endGUI extends GUI{
 
     public endGUI() {
         super();
-        //TODO Auto-generated constructor stub
-
     }
 
-    void drawGui(){
+    @Override
+    void drawGui()
+    {
         
-    }
-
-
-/*    void drawEndGui(Pane layerPane)
-    {
         Text endText = new Text("Game Over");
-        endText.setFont("Arial", 20);
-    }
+        endText.setFont(Font.font("Arial", 20));
     
-    Button startAgainButton = new Button("");
-    startAgainButton.setOnAction(new EventHandler <ActionEvent>)
-    {
-            @Override public void handle(ActionEvent e) {
-                endGUI.removeGui();
-                StartGUI.drawStartGUI(Pane layerPane);
+    
+        Button startAgainButton = new Button("");
 
-   */              
+        startAgainButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                 
+                /*@Override public void handle(ActionEvent e) {
+                StartGUI.drawStartGUI(Pane layerPane); //this.getLayer()
+
+
+                }*/
+            }
+        });
+    }
+           
 
 }
